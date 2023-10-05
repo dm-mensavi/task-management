61df270b29171f9132254f0fbaf6e78645b57651
dm-mensavi <mensavidavid@gmail.com>
2023-10-05 00:35:36 +0000

    Add task service and model

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Management API')
+    .setDescription('An API for managing your daily tasks')
+    .setVersion('1.0')
+    .addTag('task-management')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
